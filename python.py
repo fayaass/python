62e7ab03f985813c4bb425b41aa4ff63dfbe5f25
fayas <muhammedfayas59504@gmail.com>
2024-09-05 09:18:20 +0530

    new

--- python.py
+++ python.py
@@ -1309,9 +1309,8 @@ else:
 
 
 numbers={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
-a=int(input("enter a number :"))
+
 for i in numbers:
-    if i[a]:
         print(i)
 
 
