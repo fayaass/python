26d56726d3570e694ca679753eeaf256c22973e7
fayas <muhammedfayas59504@gmail.com>
2024-09-05 09:01:56 +0530

    new

--- python.py
+++ python.py
@@ -1186,76 +1186,135 @@ else:
 
 #task1
 
-fwr=[['shoe',1,8,'adidas',2999,6],['slider',2,7,'nike',1299,8],['boots',3,9,'woodland',5999,7]]
-while True:
- print('''
-1.product details
-2.view product details
-3.upadte product details
-4.search product details
-5.delete product details
-6.exit
-          ''')
+# fwr=[['shoe',1,8,'adidas',2999,6],['slider',2,7,'nike',1299,8],['boots',3,9,'woodland',5999,7]]
+# while True:
+#  print('''
+# 1.product details
+# 2.view product details
+# 3.upadte product details
+# 4.search product details
+# 5.delete product details
+# 6.exit
+#           ''')
  
- choice=int(input("enter your choice :"))
- if choice==1:
-         product=str(input("enter product :"))
-         id=int(input("enter id :"))
-         size=int(input("enter size :"))
-         brand=str(input("enter brand :"))
-         price=str(input("enter price :"))
-         stock=int(input("enter stock :"))
-         fwr.append([product,id,size,price,brand,stock])
- elif choice==2:
-      for i in fwr:
-           print(i)
- elif choice==3:
-    id=int(input("enter id :"))
-    f=0
-    for i in fwr:
-        if id in i:                                                                            
-            while True:
-                 print('''
-                    1.product
-                    2.size''')
-                 choice=int(input('enter the choice :'))
-                 if choice == 1:
-                        product=str(input('enter product :'))
-                        i[0]=product
-                    elif choice==2:
-                        size=int(input('enter size :'))
-                        i[2]=size                                                                      
-                    elif choice==3:
-                        brand=str(input('enter brand :'))
-                        i[3]=brand
-                    elif choice==4:
-                        price=int(input('enter price :'))
-                        i[4]=price
-                    elif choice==5:
-                        stock=str(input('enter stock :'))
-                        i[5]=stock
-                    f=1
-    if f==0:
-                print('invalid name')
- elif choice==4:
-      id=int(input("enter id :"))
-      f=0
-      for i in fwr:
-           if id in i:
-             print(i)
-             f=1
-      if f==0:
-               print('invalid name')
- elif choice==5:
-      id=int(input("enter id :"))
-      f=0
-      for i in fwr:
-           if id in i:
-             fwr.remove(i)
-             f=1
-      if f==0:
-               print('invalid name')
- elif choice==6:
-     break
- else:
-     print('invalid choice')
+#  choice=int(input("enter your choice :"))
+#  if choice==1:
+#          product=str(input("enter product :"))
+#          id=int(input("enter id :"))
+#          size=int(input("enter size :"))
+#          brand=str(input("enter brand :"))
+#          price=str(input("enter price :"))
+#          stock=int(input("enter stock :"))
+#          fwr.append([product,id,size,price,brand,stock])
+#  elif choice==2:
+#       for i in fwr:
+#            print(i)
+#  elif choice==3:
+#     id=int(input("enter id :"))
+#     f=0
+#     for i in fwr:
+#         if id in i:                                                                            
+#             while True:
+#                  print('''
+#                     1.product
+#                     2.size''')
+#                  choice=int(input('enter the choice :'))
+#                  if choice == 1:
+#                         product=str(input('enter product :'))
+#                         i[0]=product
+#                     elif choice==2:
+#                         size=int(input('enter size :'))
+#                         i[2]=size                                                                      
+#                     elif choice==3:
+#                         brand=str(input('enter brand :'))
+#                         i[3]=brand
+#                     elif choice==4:
+#                         price=int(input('enter price :'))
+#                         i[4]=price
+#                     elif choice==5:
+#                         stock=str(input('enter stock :'))
+#                         i[5]=stock
+#                     f=1
+#     if f==0:
+#                 print('invalid name')
+#  elif choice==4:
+#       id=int(input("enter id :"))
+#       f=0
+#       for i in fwr:
+#            if id in i:
+#              print(i)
+#              f=1
+#       if f==0:
+#                print('invalid name')
+#  elif choice==5:
+#       id=int(input("enter id :"))
+#       f=0
+#       for i in fwr:
+#            if id in i:
+#              fwr.remove(i)
+#              f=1
+#       if f==0:
+#                print('invalid name')
+#  elif choice==6:
+#      break
+#  else:
+#      print('invalid choice')
+
+#-----#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# data=[{'name':'a','age':20},{'name':'b','age':30},{'name':'c','age':45}]
+
+# # for i in data:
+# #     print(i['age'])
+
+# print('{:<10}{:<10}'.format('name','age'))
+# print('-'*20)
+# for i in data:
+#     if i['age']<=30:
+#         print('{:<10}{:<10}'.format(i['name'],i['age']))
+
+# print('{:<10}{:<10}'.format('name','age'))
+# print('-'*20)
+# for i in data:
+#     if i['age']>30:
+#         print('{:<10}{:<10}'.format(i['name'],i['age']))
+
+
+# data=[{'name':'a','age':22},{'name':'c','age':20},{'name':'b','age':30},{'name':'c','age':45}]
+# a=[]
+# b=[]
+# for i in data:
+#     if i['age']<=30:
+#         a.append(i)
+#     else:
+#         b.append(i)
+# print(a)
+# print(b)
+
+
+# data=[{'name':'a','age':22},{'name':'c','age':20},{'name':'b','age':30},{'name':'c','age':45}]
+# a=[i for i in data if i ['age']<=30]
+# b=[i for i in data if i ['age']>30]
+# print(a)
+# print(b)
+
+
+# l=[1,2,3,4,5,6,7,8,9,10]
+# even=[i for i in l if i%2==0]
+# odd=[i for i in l if i%2!=0]
+# print(even)
+# print(odd)
+
+
+
+numbers={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+a=int(input("enter a number :"))
+for i in numbers:
+    if i[a]:
+        print(i)
+
+
+
+
+
