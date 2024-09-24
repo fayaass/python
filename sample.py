4bf955083bcca969aa67fb18434cc809d9ba93a5
fayaass <muhammedfayas59504@gmail.com>
2024-09-24 08:42:49 +0530

    files added

---
+++ sample.py
@@ -0,0 +1,29 @@
+# import python as b
+
+# b.sample()
+
+# print(b.a)
+
+    #  syntax >>>       import mod_name
+    #                   mod_name.func_name()
+
+
+    #         from_mod_name import func_name var
+    #                   func_name
+
+    #                from mod_name import *
+    #                   func_name
+
+
+
+
+
+# from python import sample,a
+
+# sample()
+# print(a)
+
+
+# from python import *
+# sample()
+# print(a)
